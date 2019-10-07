3b08ab50f971dc314e4072d9c05e6c6fd1445e4c
wzf6667 <1207343523@qq.com>
2019-10-07 20:52:23 +0800

    good

---
+++ src/main/java/tut6/Calc.java
@@ -0,0 +1,16 @@
+package tut6;
+
+public class Calc {
+	public static void main(final String[] args){
+		add(1,2);
+		subtract (1,2);
+	}
+	public static int add (int a,int b) {
+		return a+b;
+	}
+	public static int subtract (int a,int b) {
+		return a-b;}
+	public static int time (int a,int c) {
+			return a*c;
+	}
+}
